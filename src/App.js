c66d48c11a937ebe2b3f0d7f94d805de50e49372
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-10-17 11:29:22 -0300

    App function component

--- src/App.js
+++ src/App.js
@@ -4,34 +4,33 @@ import Nav from './components/nav'
 import Table from './components/table'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-class App extends React.Component { 
-  render() {
-    return (
-      <Router>
-        <div id="container">
-          <header><span>Base de Datos Aplicada I</span></header>
-          <nav>
-            <Nav />
-          </nav>
-          <main>
-            <Switch>
-              <Route path="/cliente" 
-              render={(routerProps) => (<Table {...routerProps} url = 'cliente'/>)}
-              />
-              <Route path="/escribano" 
-              render={(routerProps) => (<Table {...routerProps} url = 'escribano'/>)}
-              />
-              <Route path="/localidad" 
-              render={(routerProps) => (<Table {...routerProps} url = 'localidad'/>)}
-              />
-            </Switch> 
-          </main>
-          <footer><span>Azzaretti && Carracedo</span></footer> 
-        </div>   
-      </Router>     
-    )
-  }
+function App () { 
+  return (
+    <Router>
+      <div id="container">
+        <header><span>Base de Datos Aplicada I</span></header>
+        <nav>
+          <Nav />
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/cliente" 
+            render={(routerProps) => (<Table {...routerProps} url = 'cliente'/>)}
+            />
+            <Route path="/escribano" 
+            render={(routerProps) => (<Table {...routerProps} url = 'escribano'/>)}
+            />
+            <Route path="/localidad" 
+            render={(routerProps) => (<Table {...routerProps} url = 'localidad'/>)}
+            />
+          </Switch> 
+        </main>
+        <footer><span>Azzaretti && Carracedo</span></footer> 
+      </div>   
+    </Router>     
+  )
 }
+
 
 export default App;
 
